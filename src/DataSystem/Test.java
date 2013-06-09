10322ed073d637d21f6f83997e0025a1ebba7d8c
Russell <rlw82@drexel.edu>
2013-06-08 23:35:07 -0400

    update

--- src/DataSystem/Test.java
+++ src/DataSystem/Test.java
@@ -1,6 +1,8 @@
 package DataSystem;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class Test extends Survey
@@ -30,12 +32,24 @@ public class Test extends Survey
 	public void create() throws IOException
 	{
 		super.create();
+		createCorrectAnswers();
+	}
+
+	private void createCorrectAnswers()
+	{
 		if (questions.size() != 0)
 		{
 			for(int i = 0; i < questions.size(); i++)
 			{
 				Question tempQues = questions.get(i);
-				if(tempQues.getType().equals("Essay") || tempQues.getType().equals("ShortAnswer"))
+				if(tempQues.getType().equals("MultipleChoice") || tempQues.getType().equals("ShortAnswer"))
+				{
+					System.out.println("How many answers does this Question have?");
+					{
+						BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+						String temp = br.readLine()
+					}
+				}
 			}
 		}
 		else
